277734704f88543493ce0296ab09184c80568b68
JoshuaFogelson <joshfogelson@gmail.com>
2020-10-07 18:36:51 -0700

    shaders

--- lab/shaders/frag.rgb.glsl
+++ lab/shaders/frag.rgb.glsl
@@ -1,1 +1,10 @@
 // We will do this together in class (watch the Zoom recording if you missed it!).
+precision mediump float;
+
+varying vec3 fragColor;
+varying vec3 fragNormal;
+
+// light
+uniform vec3 ambientLight;
+uniform vec3 lightDirection;
+uniform vec3 lightIntensity;
